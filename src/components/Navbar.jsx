9085f5af41f315d657c381efb39a5ae8d9efc6d5
Malka Anjum <malkaanjum1999@gmail.com>
2026-07-18 22:27:59 +0530

    navbar responsiveness done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import { FaXmark } from 'react-icons/fa6'
 
 const Navbar = () => {
+    const [ showMenu, setShowMenu ] = useState(false);
+
     return (
         <nav className='fixed w-full z-50 bg-dark-100/90 backdrop-blur-sm py-4 px-8 shadow-lg'>
             <div className="container mx-auto flex justify-between items-center">
@@ -47,7 +51,53 @@ const Navbar = () => {
                         <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
                     </a>
                 </div>
+
+                {/* For mobile screen */}
+                <div className='md:hidden'>
+                    {
+                        showMenu ? 
+                        <FaXmark onClick={() => setShowMenu(!showMenu)} className='text-2xl cursor-pointer' /> :
+                        <FaBars onClick={() => setShowMenu(!showMenu)} className='text-2xl cursor-pointer' />
+                    }
+                </div>
             </div>
+
+            {/* for mobile screen */}
+                {
+                    showMenu && (
+                        <div className='md:hidden mt-4 bg-dark-300 h-screen rounded-lg p-2 flex flex-col space-y-4 justify-center items-center'>
+                             <a href="#home" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>Home</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+
+                            <a href="#about" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>About</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+
+                            <a href="#skills" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>Skills</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+
+                            <a href="#projects" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>Projects</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+
+                            <a href="#experience" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>Experience</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+
+                            <a href="#contact" className="relative text-white/80 transition duration-300 hover:text-purple group">
+                                <span>Contact</span>
+                                <span className='absolute left-0 -bottom-1 w-0 h-0.5 bg-purple transition-all duration-300 group-hover:w-full'></span>
+                            </a>
+                        </div>
+                    )
+                }
         </nav>
     )
 }
